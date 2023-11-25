c425dc103ff806b3464ef34e84897fbb1be1bab5
“AshishJha30” <ashishjha2000.aj@gmail.com>
2023-11-26 02:13:45 +0530

    fixed height issue on login page

--- Projects/React/React_Projects/Study_Notion/src/pages/Dashboard.js
+++ Projects/React/React_Projects/Study_Notion/src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='flex justify-center items-center text-white text-3xl h-full'>
+    <div className='flex h-screen justify-center items-center text-white text-3xl'>
       Welcome to StudyNotion!!
     </div>
   )
